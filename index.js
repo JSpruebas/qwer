fe474b14647ef8f397c37bcc8bf4c8b5a95054bc
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-06 18:20:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
+
+let web3 = new Web3('https://bsc-dataseed.binance.org/');
+console.log(web3);
+
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
     // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
-
-let web3 = new Web3('https://bsc-dataseed.binance.org/');
-console.log(web3);
