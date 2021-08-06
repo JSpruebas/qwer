854da8efbcc2e182b687949cc0358ddd5315a2e5
sefalafa <76005790+sefalafa@users.noreply.github.com>
2021-08-06 18:29:44 -0300

    .-

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
+import Web3 from './node_modules/web3/dist/web3.min.js';
+
+  const getWeb3 = async () => {
+    const provider = await detectEthereumProvider({timeout: 10000})
+    if (provider) {
+      provider.on('chainChanged', () => location.reload())
+      provider.on('networkChanged', () => location.reload())
+      provider.on('accountsChanged', () => location.reload())
+      provider.on('disconnect', () => location.reload())
+  
+      await provider.request({ method: 'eth_requestAccounts' })
 
 
-let web3 = new Web3('https://bsc-dataseed.binance.org/');
-console.log(web3);
+      web3 = new Web3(provider)
 
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+     
+  
+      const result = {
+        injectedWeb3: true,
+        provider,
+        web3 () {
+          return web3
+        }
+      }
+      result.networkId = await web3.eth.net.getId()
+      result.coinbase = await web3.eth.getCoinbase()
+      result.balance = await web3.eth.getBalance(result.coinbase)
+      LocalStorage.set('account-connected', 'true')
+      return result
+    } 
+    console.error('Web3 provider not detected')
+    return {injectedWeb3: false}
   }
+
+  getWeb3();
+
+  const coso = async () => {
+
+    const resultado = await web3.eth.getBalance(0x79e858dFAB69949F54D22b3cCCBC04499bF68532)
+
+}
+
+  coso();
+
+
+  var zeroStratContract = new web3.eth.Contract(zeroStratAbi, "0xaafAb69eC1984c43dE9720F20743033B04E09aFA");
+  zeroStratContract.methods.calculateTotalPendingCakeRewards().call().then(console.log);
